71f098af1e061ed5c42970cc2c59bb63fdd8b300
rv1n <swein1258@gmail.com>
2026-03-04 11:02:20 +0300

    fix logger button

--- services/price_logger.py
+++ services/price_logger.py
@@ -85,11 +85,18 @@ class PriceLogger:
             skipped_count = 0
             # Одна общая метка времени для всех записей текущего запуска
             log_time = datetime.now(self.moscow_tz)
-            # Готовим bulk-запросы к MOEX для акций и облигаций
-            stock_tickers = [t for t, info in unique_tickers.items() if info['instrument_type'] == 'STOCK']
-            bond_tickers = [t for t, info in unique_tickers.items() if info['instrument_type'] == 'BOND']
-            bulk_prices_stock = self.moex_service.get_bulk_prices(stock_tickers, 'STOCK') if stock_tickers else {}
-            bulk_prices_bond = self.moex_service.get_bulk_prices(bond_tickers, 'BOND') if bond_tickers else {}
+
+            # Готовим bulk-запросы к MOEX для акций и облигаций.
+            # Для ручного логирования (force=True) используем поштучные запросы,
+            # чтобы задействовать полную логику get_current_price для «особых» инструментов (CNYM, GOLD, LQDT и т.п.).
+            if not force:
+                stock_tickers = [t for t, info in unique_tickers.items() if info['instrument_type'] == 'STOCK']
+                bond_tickers = [t for t, info in unique_tickers.items() if info['instrument_type'] == 'BOND']
+                bulk_prices_stock = self.moex_service.get_bulk_prices(stock_tickers, 'STOCK') if stock_tickers else {}
+                bulk_prices_bond = self.moex_service.get_bulk_prices(bond_tickers, 'BOND') if bond_tickers else {}
+            else:
+                bulk_prices_stock = {}
+                bulk_prices_bond = {}
             
             # Логируем цену для каждого уникального тикера
             for ticker, ticker_info in unique_tickers.items():
